06a1991ddce456f9b1985155ceb0d656281f7757
Ana <ananyagopal61@gmail.com>
2018-04-16 20:34:22 -0400

    Update Testcases.py

--- Testcases.py
+++ Testcases.py
@@ -3,57 +3,41 @@ import matplotlib.patches as patches
 import math
 
 fig1 = plt.figure()
-fig2 = plt.figure()
-
-
 
 ax1 = fig1.add_subplot(111, aspect='equal')
 
+axes = plt.gca()
+axes.set_xlim([-3,10])
+axes.set_ylim([-3,10])
+radius = 2
 x = 0
 y = 0
-for itr in range(0, 3):
-	print x
-	ax1.add_patch(
-	    patches.RegularPolygon(
-		(x, y),     # (x,y)
-		6,              # number of vertices
-		0.1,
-		color='red'            # radius
-	    )
-	)
-	y=y+0.3
 
-y = 0.15
-newx = math.sqrt(3)*0.5*0.1
-print newx
+for itr in range (0,1):
+    
+    ax1.add_patch(
+        patches.RegularPolygon(
+            (x,y),
+            6,
+            radius,
+            edgecolor = 'black'
+        )
+    )
 
-for itr in range(0, 3):
-	
-	ax1.add_patch(
-	    patches.RegularPolygon(
-		(newx, y),     # (x,y)
-		6,              # number of vertices
-		0.1,
-		color='blue'            # radius
-	    )
-	)
-	y=y+0.3
-
-'''
-y = 0
-newxx = math.sqrt(3)*0.5*newx
+distanceToside = math.sqrt(3)/4*radius*radius;
+# Test cases
 
 
-for itr in range(0, 3):
-	
-	ax1.add_patch(
-	    patches.RegularPolygon(
-		(newxx, y),     # (x,y)
-		6,              # number of vertices
-		0.1,
-		color='red'            # radius
-	    )
-	)
-	y=y+0.3
-'''	
-fig1.savefig('reg-polygon1d.png', dpi=90, bbox_inches='tight')
+test = [
+            [1,1], [0.0,1.2], [-2.3,0], [0.77,0.76], [1.0,1.2], [x+distanceToside, y], [x, y+distanceToside], [x,y-distanceToside], [x-distanceToside,y], [x-distanceToside+0.9,y],
+            [x-distanceToside,y+0.9]
+            
+       ]
+ax1.plot([x], [y], marker='o', markersize=3, color="red")
+for d in range(0,11):
+    #for ata in range(0,2):
+    print(test[d])
+    ax1.plot([test[d][0]], [test[d][1]], marker='o', markersize=3, color="black")
+
+
+fig1.savefig('figure.png', dpi=90, bbox_inches='tight')
